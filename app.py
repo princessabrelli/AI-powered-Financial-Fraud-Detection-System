825440a3a9b346e9d0c3554618cd49595a40dce5
Kevin Gonzales <kevingonzales1112@gmail.com>
2024-09-28 14:43:36 -0400

    Added Flask API

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
